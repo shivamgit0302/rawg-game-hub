674e9ca5bd3384aa4253e163bee7352c1e1acef2
Shivam Tiwari <shivam@aimedis.com>
2023-11-03 17:07:14 +0400

    default color mode and no image imp

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import { ChakraProvider, theme, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
+import theme from "./components/Theme";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
@@ -12,7 +13,7 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript />
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
