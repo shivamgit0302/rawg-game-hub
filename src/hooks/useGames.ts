b92bd7174a199378e5ca77a18cd9cb662ea1937b
Shivam Tiwari <shivam@aimedis.com>
2023-11-07 14:36:40 +0400

    test api

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -101,6 +101,23 @@ const useGamesQuery = (gamesQuery:GamesQuery) => {
     });
   }, []);
 
+  useEffect(() => {
+    axios.get('https://metaverse.aidoc.io/api/world/list' , {
+      headers: {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsImtpZCI6IjhqWm9sSTBpOHd2bEp1VUEiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjk5NjU3ODM5LCJpYXQiOjE2OTkzNTMwMzksImlzcyI6Imh0dHBzOi8veHZibHVxcnhiaXJodHd0c253Zmkuc3VwYWJhc2UuY28vYXV0aC92MSIsInN1YiI6IjI1ODRlZjZjLTg1ZDItNGJkOS04YjJiLWIxODEzMjdkMWQzYSIsImVtYWlsIjoic2hpdmFtQGFpbWVkaXMuY29tIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJlbWFpbCIsInByb3ZpZGVycyI6WyJlbWFpbCJdfSwidXNlcl9tZXRhZGF0YSI6e30sInJvbGUiOiJhdXRoZW50aWNhdGVkIiwiYWFsIjoiYWFsMSIsImFtciI6W3sibWV0aG9kIjoicGFzc3dvcmQiLCJ0aW1lc3RhbXAiOjE2OTkzNTMwMzl9XSwic2Vzc2lvbl9pZCI6Ijg1NjRhODVmLTM4M2EtNGYxZS1hOTJiLWYzMTExMzE4ZDVlMyJ9.88mueZGyknyDUXGHpcgbf27XXoyHZ45-mB2qh0xxIF8',
+        // Add any other necessary headers here
+      },
+    })
+    .then((response) => {
+      console.log(response.data , 'data')
+      console.log(response , 'entire response')
+    })
+    .catch((err) => {
+      console.log(err.message, 'error message')
+      console.log(err, 'entire error')
+    });
+  }, []);
+
 
   return {gameList, isLoading , error }
 
